1d1efab4b4d3d18b52f31abc289df2cdd31a3f44
Conor Hope <c_hope@hotmail.co.uk>
2022-03-11 13:52:29 +0000

    Corrected spelling

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,11 +10,11 @@ describe 'fizzbuzz' do
     expect(5.fizzbuzz).to eq 'buzz'
   end
 
-  it 'returns "fizzbuzz" for a number dividable by 3 and 5' do
+  it 'returns "fizzbuzz" for a number divisible by 3 and 5' do
     expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
 
-  it 'returns integer if the integer is not divisible by 3 or 5' do
+  it 'returns integer if the integer is not divisible by 3, 5 or both' do
     expect(4.fizzbuzz).to eq 4
   end
 end
