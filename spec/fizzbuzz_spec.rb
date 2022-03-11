09a0f5bd55b2249b8c5ee475b2235a5ce56306a7
Conor Hope <c_hope@hotmail.co.uk>
2022-03-11 12:07:05 +0000

    3 tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,20 @@
 require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns "fizz" for the number 3' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" for a number dividable by 3 and 5' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+end
+
