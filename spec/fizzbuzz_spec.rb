2cebb1db42846f5f8259b99b76d6677b227244fe
Joe <j.newman@live.co.uk>
2022-03-11 12:17:56 +0000

    Complete?

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,20 +1,20 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" for the number 3' do
+  it 'returns "fizz" for integers which are divisible by 3' do
     expect(3.fizzbuzz).to eq 'fizz'
+    expect(6.fizzbuzz).to eq 'fizz'
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "buzz" for the number 5' do
     expect(5.fizzbuzz).to eq 'buzz'
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "fizzbuzz" for a number dividable by 3 and 5' do
     expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
-end
 
+  it 'returns integer if the integer is not divisible by 3 or 5' do
+    expect(4.fizzbuzz).to eq 4
+  end
+end
